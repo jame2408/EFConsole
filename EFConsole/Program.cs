f0b9d71658b57df859c211715842ae01f69f5c24
jame2408 <jame2408@gmail.com>
2016-05-14 13:23:17 +0800

    edmx操作與撰寫CRUD程式

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -12,7 +12,35 @@ namespace EFConsole
         {
             using (var db = new ContosoUniversityEntities())
             {
+                //foreach (var item in db.Course)
+                //{
+                //    Console.WriteLine(item.Title);
+                //}
+
+                //var c = new Course()
+                //{
+                //    Title = "Git Test",
+                //    Credits = 4
+                //};
+                //c.Department = db.Department.Find(2);
+                //db.Course.Add(c);
+
+                //Console.WriteLine(c.CourseID);
+                //db.SaveChanges();
+                //Console.WriteLine(c.CourseID);
+                //Console.ReadLine();
+
+                db.Course.Remove(db.Course.Find(8));
                 foreach (var item in db.Course)
+                {
+                    item.Credits += 1;
+                }
+                db.SaveChanges();
+
+                var data = from p in db.Course
+                           where p.Title != Guid.NewGuid().ToString()
+                           select p;
+                foreach (var item in data)
                 {
                     Console.WriteLine(item.Title);
                 }
