70fa2408e0c18f8bfdea951b27bd7b7356bc403c
jame2408 <jame2408@gmail.com>
2016-05-14 16:27:22 +0800

    各式查詢語法

--- EFConsole/ContosoUniversity.Context.cs
+++ EFConsole/ContosoUniversity.Context.cs
@@ -30,5 +30,6 @@ namespace EFConsole
         public virtual DbSet<Enrollment> Enrollment { get; set; }
         public virtual DbSet<OfficeAssignment> OfficeAssignment { get; set; }
         public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<vwDeptCourseCount> vwDeptCourseCount { get; set; }
     }
 }
--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -1,23 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EFConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using (var db = new ContosoUniversityEntities())
             {
                 #region LINQ to Entities 寫法
-                //var one = db.Course.Include("Department").FirstOrDefault(s => s.CourseID == 1);                
+
+                //var one = db.Course.Include("Department").FirstOrDefault(s => s.CourseID == 1);
                 //Console.WriteLine(one?.Title + ":" + one?.Department.Name);
-                #endregion
+
+                #endregion LINQ to Entities 寫法
 
                 #region 雙重迴圈寫法
+
                 //foreach (var item in db.Department)
                 //{
                 //    Console.WriteLine(item?.Name + "：");
@@ -26,32 +26,49 @@ namespace EFConsole
                 //        Console.WriteLine("    " + data?.Title);
                 //    }
                 //}
-                #endregion
+
+                #endregion 雙重迴圈寫法
 
                 #region Native SQL 寫法
-                var sql = @"SELECT 
-                                Department.Name as DeptName, 
-                                COUNT(*) AS CourseCount
-                            FROM Course 
-                            INNER JOIN Department 
-                                ON Course.DepartmentID = Department.DepartmentID
-                            GROUP BY Department.Name";
-                var data = db.Database.SqlQuery<DeptCourseCount>(sql);
-                foreach (var item in data)
+
+                //var sql = @"SELECT
+                //                Department.Name as DeptName,
+                //                COUNT(*) AS CourseCount
+                //            FROM Course
+                //            INNER JOIN Department
+                //                ON Course.DepartmentID = Department.DepartmentID
+                //            GROUP BY Department.Name";
+                //var data = db.Database.SqlQuery<DeptCourseCount>(sql);
+                //foreach (var item in data)
+                //{
+                //    Console.WriteLine(item?.DeptName + ":" + item?.CourseCount);
+                //}
+
+                #endregion Native SQL 寫法
+
+                #region view 寫法(將Native SQL改放置view中)
+
+                var data = db.vwDeptCourseCount;
+                //判斷集合是否有值，使用Any()，效能最好
+                if (data.Any())
                 {
-                    Console.WriteLine(item?.DeptName + ":" + item?.CourseCount);
+                    foreach (var item in data)
+                    {
+                        Console.WriteLine(item?.DeptName + ":" + item?.CourseCount);
+                    }
                 }
+
                 #endregion
             }
         }
 
-        class DeptCourseCount
+        private class DeptCourseCount
         {
             public string DeptName { get; set; }
             public int CourseCount { get; set; }
         }
 
-        static void EF基本練習(string[] args)
+        private static void EF基本練習(string[] args)
         {
             using (var db = new ContosoUniversityEntities())
             {
