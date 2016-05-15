f117d0421448243f55be0ee4af6d5a1e23db49e0
jame2408 <jame2408@gmail.com>
2016-05-15 17:44:29 +0800

    使用擴充套件實作CodeFirsrFormDateBase

---
+++ POCOGen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCOGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContosoUniversityEntities())
+            {
+                var data = db.GetDept();
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item.DeptName);
+                }
+            }
+        }
+    }
+}
